69e7eab87864687cdd05e81b759530223cbc3f87
adoniaslemos <adoniaslg@gmail.com>
2019-04-26 12:00:02 -0300

    limiarização das imagens

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-from tkinter import *
+import cv2
+import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
 
-class Application:
-    def __init__(self, master=None):
-        self.widget1 = Frame(master)
-        self.widget1.pack()
+def show_image():
 
+    while True:
+        # carregando as imagens
+        img = cv2.imread("imagens/roi.png", -1)
+        img_not_glaucoma = cv2.imread("imagens/not_glaucoma.png", -1)
 
-        self.msg = Label(self.widget1, text="Hello World")
-        self.msg["font"] = ("Roboto", "15 ", "bold")
-        self.msg.pack()
+        # transformando as imagens carregadas para a escala de cinza
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_not_glaucoma_gray = cv2.cvtColor(img_not_glaucoma, cv2.COLOR_BGR2GRAY)
 
-        # botão sair
-        self.sair = Button(self.widget1)
-        self.sair["text"] = "Sair"
-        self.sair["font"] = ("Calibri", "10")
-        self.sair["width"] = 5
-        self.sair["command"] = self.widget1.quit
-        self.sair.pack()
+        # limiarizando a imagem
+        ret, thresh1 = cv2.threshold(img_gray, 205, 255
+                                     , cv2.ADAPTIVE_THRESH_MEAN_C)
+        ret, thresh2 = cv2.threshold(img_not_glaucoma_gray, 205, 255
+                                     , cv2.ADAPTIVE_THRESH_MEAN_C)
+        # exibindo a imagens do paciente com glaucoma
+        cv2.imshow('Com Glaucoma (Segmentado)', thresh1)
+        cv2.imshow('Com Glaucoma', img)
 
-root = Tk()
+        # exibindo a imagens do paciente sem glaucoma
+        cv2.imshow('Sem Glaucoma (Segmentado)', thresh2)
+        cv2.imshow('Sem Glaucoma', img_not_glaucoma)
 
-Application(root)
+        cv2.waitKey(1)
 
-root.mainloop()
+show_image()
---
+++ utils.py
@@ -0,0 +1,26 @@
+from tkinter import *
+import cv2
+
+class Application:
+    def __init__(self, master=None):
+        self.widget1 = Frame(master)
+        self.widget1.pack()
+
+
+        self.msg = Label(self.widget1, text="Hello World")
+        self.msg["font"] = ("Roboto", "15 ", "bold")
+        self.msg.pack()
+
+        # botão sair
+        self.sair = Button(self.widget1)
+        self.sair["text"] = "Sair"
+        self.sair["font"] = ("Calibri", "10")
+        self.sair["width"] = 30
+        self.sair["command"] = self.widget1.quit
+        self.sair.pack()
+
+root = Tk()
+
+Application(root)
+
+root.mainloop()
