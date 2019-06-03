b917686fd1f6a585e7de21aff56902f85e5192b2
adoniaslemos <adoniaslg@gmail.com>
2019-06-03 17:58:45 -0300

    frequencia imagem

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # AADG
 
-Api para auxiliar no diagnóstico de glaucoma utilizando exames de fundo de olho. Software desenvolvido para a disciplina de Sistemas Multimídia no Centro Universitário 7 de Setembro.
+API para auxiliar no diagnóstico de glaucoma utilizando exames de fundo de olho. Software desenvolvido para a disciplina de Sistemas Multimídia no Centro Universitário 7 de Setembro.
 
 ## Bibliotecas
-- opencv
-- numpy
 
-    pip install -r requirements.txt
+- python => 3.6 ou maior
+- opencv-python => 4.1.0
+- numpy => 1.16
+- matplotlib => 
+
+
+
+## Instalação das Bibliotecas 
+    $ pip install -r requirements.txt
 
 ## Orientação
 Prof° Edson
 
 ## Contribuidores
-- Adonias Lemos
+- [Adonias Lemos](https://github.com/AdoniasLemos)
 - Alessandra Santos
-- Kézia Brasil
+- [Kézia Brasil](https://github.com/kezbrasiil)
--- aadg/classificador.py
+++ aadg/classificador.py
@@ -9,17 +9,18 @@ def diagnostic(path_image):
     image = cv2.imread(path_image)
 
     image_converted = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
     image_blur = cv2.GaussianBlur(image_converted, (5, 5), 0)
 
-    ret, th = cv2.threshold(image_blur,190, 255,cv2.THRESH_BINARY)
+    ret, th = cv2.threshold(image_blur, 205, 255, cv2.THRESH_BINARY)
 
     kernel = np.ones((5, 5), np.uint8)
 
     image_dilated = cv2.dilate(th, kernel, iterations=1)
     image_eroded = cv2.erode(image_dilated, kernel, iterations=1)
-
+    cv2.imshow("Image", image_eroded)
+    cv2.waitKey()
     media =  calc_media(image_eroded)
-    print(media)
 
 
 def calc_media(image):
@@ -29,7 +30,11 @@ def calc_media(image):
 
     hist = cv2.calcHist([image], [0], None, [256], [0, 256])
 
-    for i in range(205, 255):
+    for i in range(255):
         divisor= divisor+ 1
         freq += hist[i]
+        print(hist[i])
     return freq / divisor
+
+
+diagnostic('imagens/roi.png')
